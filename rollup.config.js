c54905dcfe238901a774a9a3ec3d14edc2a30bac
Jacob Buck <buck.jacob@gmail.com>
2020-11-14 03:01:18 +0000

    Update rollup.config.js: - Add ES Module build to output - Rename CommonJS build - Generate source maps

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,19 @@ import babel from 'rollup-plugin-babel';
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'lib/index.js',
-    format: 'cjs',
-  },
+  output: [
+    {
+      exports: 'default',
+      file: 'lib/index.cjs.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'lib/index.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
   external: ['escape-string-regexp'],
   plugins: [babel()],
 };
