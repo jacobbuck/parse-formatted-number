2152e16fa21375cd281395ccf82d7236bf891c70
Jacob Buck <buck.jacob@gmail.com>
2020-11-14 06:29:16 +0000

    Add type checking to parameters

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 const parseFormattedNumber = (value, options = {}) => {
+  if (process.env.NODE_ENV !== 'production') {
+    if (typeof options !== 'object') {
+      throw new TypeError(
+        `Expected \`options\` to be of type \`object\` but received type \`${typeof options}\``
+      );
+    }
+    if (typeof options.decimal !== 'string' && options.decimal !== undefined) {
+      throw new TypeError(
+        `Expected property \`decimal\` to be of type \`string\` but received type \`${typeof options.decimal}\` in \`options\``
+      );
+    }
+    if (typeof options.decimal === 'string' && options.decimal.length !== 1) {
+      throw new TypeError(
+        `Expected property \`decimal\` to have length \`1\`, got \`${options.decimal}\` in \`options\``
+      );
+    }
+  }
+
   const { decimal = '.' } = options;
 
   if (Number.isFinite(value)) {
